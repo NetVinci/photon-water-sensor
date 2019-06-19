f50621b861f3e0c34aeb8c018cbadacc2bbe1ba4
Jared Henry <jh75707@gmail.com>
2019-06-19 09:32:28 -0500

    Added 3Sensors

--- sensor.ino
+++ sensor.ino
@@ -4,43 +4,57 @@
 // This #include statement was automatically added by the Particle IDE.
 #include "elapsedMillis/elapsedMillis.h"
 
-String _version = "0.04";
+// Version of the code that is being used.
+String _version = "0.05";
 
-//this reads the flood sensor every 2 seconds
+//this reads the flood sensors every 2 seconds
 #define FLOOD_READ_INTERVAL 2000
 
 //this defines the frequency of the notifications sent to the user
-#define FLOOD_FIRST_ALARM 10000 //10 seconds
-#define FLOOD_SECOND_ALARM 60000 //1 minute
-#define FLOOD_THIRD_ALARM 300000 //5 minutes
-#define FLOOD_FOURTH_ALARM 900000 //15 minutes
-#define FLOOD_FIFTH_ALARM 3600000 //1 hour
-#define FLOOD_SIXTH_ALARM 14400000 //4 hours - and every 4 hours ever after, until the situation is rectified (ie no more water is detected)
+#define FLOOD_FIRST_ALARM 10000   //10 seconds
+#define FLOOD_SECOND_ALARM 30000  //.5 minute
+#define FLOOD_THIRD_ALARM 60000   //1 minutes
+#define FLOOD_FOURTH_ALARM 300000 //5 minutes - We will alert every 5 minutes until the sensor is dry.
 
 #define FLOOD_NOTIF "FLOOD"
 
 elapsedMillis flood_timer;
 elapsedMillis flood_alarm_timer;
 
-int flood_alarms_array[6]={FLOOD_FIRST_ALARM, FLOOD_SECOND_ALARM, FLOOD_THIRD_ALARM, FLOOD_FOURTH_ALARM, FLOOD_FIFTH_ALARM, FLOOD_SIXTH_ALARM};
+int flood_alarms_array[4] = {FLOOD_FIRST_ALARM, FLOOD_SECOND_ALARM, FLOOD_THIRD_ALARM, FLOOD_FOURTH_ALARM};
 int flood_alarm_index = 0;
 bool flood_detected = false;
 unsigned long flood_next_alarm = 0;
 
-int FLOOD_SENSOR = D0;
+// Define the sensors that will be used. More sensors can be added to a board as necessary.
+// As of now there are only 3 sensors connected.
+int FLOOD_SENSOR_1 = D0;
+int FLOOD_SENSOR_2 = D1;
+int FLOOD_SENSOR_3 = D2;
+
+// LED pin. This can be connected to show a led light.
 int LED = D7;
 
-void setup() {
- pinMode(FLOOD_SENSOR, INPUT_PULLUP);
- pinMode(LED, OUTPUT);
- Spark.publish("device starting", "Firmware version: " + _version, 60, PRIVATE);
+void setup()
+{
+    // Mode of the pins are PULLUP for the sensors.
+    pinMode(FLOOD_SENSOR_1, INPUT_PULLUP);
+    pinMode(FLOOD_SENSOR_2, INPUT_PULLUP);
+    pinMode(FLOOD_SENSOR_3, INPUT_PULLUP);
+    pinMode(LED, OUTPUT);
+
+    // Publish event message
+    Spark.publish("the device starting", "Firmware version: " + _version, 60, PRIVATE);
 }
 
-void loop() {
+// Set the loop to do the flood check and read global to detect the flood and notify the users.
+void loop()
+{
 
     flood_check();
-    
-    if ( flood_detected ) {
+
+    if (flood_detected)
+    {
         flood_notify_user();
     }
 }
@@ -52,33 +66,39 @@ void loop() {
  *******************************************************************************/
 int flood_check()
 {
-    if (flood_timer < FLOOD_READ_INTERVAL) {
+    if (flood_timer < FLOOD_READ_INTERVAL)
+    {
         return 0;
     }
-    
+
     //time is up, so reset timer
     flood_timer = 0;
 
-    if (not digitalRead(FLOOD_SENSOR)) {
-        
+    // Read the sensor pins for detection of the leak with the water.
+    if (not digitalRead(FLOOD_SENSOR_1) && not digitalRead(FLOOD_SENSOR_2) && not digitalRead(FLOOD_SENSOR_3))
+    {
+
         //if flood is already detected, no need to do anything, since an alarm will be fired
-        if (flood_detected){
+        if (flood_detected)
+        {
             return 0;
         }
-        
+
         flood_detected = true;
-    
+
         //reset alarm timer
         flood_alarm_timer = 0;
 
         //set next alarm
         flood_alarm_index = 0;
         flood_next_alarm = flood_alarms_array[0];
-        
-        digitalWrite(LED,HIGH);
-    } else {
-        
-        digitalWrite(LED,LOW);
+
+        digitalWrite(LED, HIGH);
+    }
+    else
+    {
+
+        digitalWrite(LED, LOW);
         flood_detected = false;
     }
     return 0;
@@ -92,28 +112,29 @@ int flood_check()
 int flood_notify_user()
 {
 
-    if (flood_alarm_timer < flood_next_alarm) {
+    if (flood_alarm_timer < flood_next_alarm)
+    {
         return 0;
     }
 
-    
     //time is up, so reset timer
     flood_alarm_timer = 0;
-    
+
     //set next alarm or just keep current one if there are no more alarms to set
-    if (flood_alarm_index < arraySize(flood_alarms_array)-1) {
+    if (flood_alarm_index < arraySize(flood_alarms_array) - 1)
+    {
         flood_alarm_index = flood_alarm_index + 1;
         flood_next_alarm = flood_alarms_array[flood_alarm_index];
     }
+
+    // Make Integration Call to twilioCall which calls a list of phone numbers to notify them a leak has been detected.
     Particle.publish("twilioCall", "A Flood Detected!!!", 60, PRIVATE);
+
+    // Make Integration Call to waterDetected which pushes a Pushover alert to the user.
     Particle.publish("waterDetected", "A Flood Detected!!!", 60, PRIVATE);
 
     //send an alarm to user (this one goes to the dashboard)
     Spark.publish(FLOOD_NOTIF, "Flood detected!", 60, PRIVATE);
- // twilioCall
-    //send an alarm to user (this one goes to pushbullet servers)
-    Spark.publish("waterDetected", "Flood detected!", 60, PRIVATE);
-    
-   
-   return 0; 
+
+    return 0;
 }
